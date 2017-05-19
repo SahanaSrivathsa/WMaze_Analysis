7b4fdb5f366a87bbeef435a392f6fcaafe6b76dc
Adele Kapellusch <akoutia@email.arizona.edu>
2017-05-19 15:28:18 -0700

    Fixed Seaborn Line Plots

--- pandas_viz/hist.py
+++ pandas_viz/hist.py
@@ -29,50 +29,28 @@ def plot_overall_barplot():
     sns.plt.title('Proportion Correct by Session', fontsize=16)
     sns.plt.show()
 
-def plot_proportion_inbound():
-    inbound = trialTypeDataframe[trialTypeDataframe['Trial Type'] == 'In'].reset_index()
 
-    old_inb = inbound[inbound['Age'] == 'Old']
-    young_inb = inbound[inbound['Age'] == 'Young']
+def plot_trialType_proportions(type):
+    colors = ['#9658b7', '#3b8437']
+    if type == 'Outbound':
+        df = trialTypeDataframe[trialTypeDataframe['Trial Type'] == 'Out'].reset_index()
+        title = 'Proportion Correct Outbound by Session'
+    if type == 'Inbound':
+        df = trialTypeDataframe[trialTypeDataframe['Trial Type'] == 'In'].reset_index()
+        title = 'Proportion Correct Inbound by Session'
 
-    new_old = old_inb.groupby('Session')['Correct/Incorrect'].mean()
-    new_young = young_inb.groupby('Session')['Correct/Incorrect'].mean()
+    old = df[df['Age'] == 'Old']
+    young = df[df['Age'] == 'Young']
+    con = pd.concat([old,young])
 
-    old_line = pd.DataFrame()
-    old_line['Proportion'] = new_old
-    old_line['Age'] = 'Old'
-
-    young_line = pd.DataFrame()
-    young_line['Proportion'] = new_young
-    young_line['Age'] = 'Young'
-
-    ln = pd.concat([old_line, young_line]).reset_index()
-    sns.pointplot(x='Session', y='Proportion', hue='Age', data=ln)
-    sns.plt.show()
-
-def plot_proportion_outbound():
-    #### NEEDS TO BE MODIFIED (no error bars)
-    outbound = trialTypeDataframe[trialTypeDataframe['Trial Type'] == 'Out'].reset_index()
-
-    old_out = outbound[outbound['Age'] == 'Old']
-    young_out = outbound[outbound['Age'] == 'Young']
-
-    new_old = old_out.groupby('Session')['Correct/Incorrect'].mean()
-    new_young = young_out.groupby('Session')['Correct/Incorrect'].mean()
-
-    old_line = pd.DataFrame()
-    old_line['Proportion'] = new_old
-    old_line['Age'] = 'Old'
-
-    young_line = pd.DataFrame()
-    young_line['Proportion'] = new_young
-    young_line['Age'] = 'Young'
-
-    ln = pd.concat([old_line, young_line]).reset_index()
-    sns.pointplot(x='Session', y='Proportion', hue='Age', data=ln)
+    sns.pointplot(x='Session', y='Correct/Incorrect',hue='Age',data=con,palette=colors,capsize=0.1)
     sns.plt.ylim(0,1)
+    sns.plt.title(title)
     sns.plt.show()
 
-plot_proportion_outbound()
+plot_trialType_proportions('Outbound')
+plot_outbound_barplot()
+
+
 
 
