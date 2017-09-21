269277d6e544dfa3671eb884aada4cfc9f1c1d29
Adele Kapellusch <akoutia@email.arizona.edu>
2017-09-21 09:21:56 -0700

    Getting back on track with mean session number plot

--- StochasticVolatility/BySession/sessionProp.py
+++ StochasticVolatility/BySession/sessionProp.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats as st
 import seaborn as sns
+import numpy as np
 
 def getData(anType,group):
+
     dir = '/Volumes/TRANS 1/BarnesLab/RawData/Processed Data/'
-    data_denom = pd.read_csv(dir + anType + group + 'Denom.csv').iloc[:,1:]# csv of total trials for each day
-    data_numAll = pd.read_csv(dir + anType + group + 'Num.csv').iloc[:,1:]# correct per day
-    out = pd.DataFrame()
-    meanprop = (data_numAll/data_denom).mean()
-    out[anType+' proportion'] = meanprop
+    data_total = pd.read_csv(dir + anType + group + 'Denom.csv').iloc[:,1:]# csv of total trials for each day
+    out = data_total
     out['age'] = group
-    out[anType+' total'] = data_denom.mean()
-    out['Session'] = range(1,len(out['age'])+1)
     return out
 
 def createListData(dataframe):
@@ -30,24 +28,39 @@ def plot(anType,group,data):
 
 
 
+inYoungTotal = getData('inbound','Young')
+inOldTotal = getData('inbound','Old')
+inbound = pd.concat([inYoungTotal,inOldTotal])
 
-inYoungProp = getData('inbound','Young')
-inOldProp = getData('inbound','Old')
+data2 = inbound.groupby(['age']).mean()
+data = inbound.groupby(['age']).describe()
 
-inbound = pd.concat([inYoungProp,inOldProp])
 
-sns.jointplot()
 
-outYoungProp = getData('outbound','Young')
-outOldProp = getData('outbound','Old')
 
-test = inYoungProp.mean()
+grouped = inbound.groupby(['age'])
+means = grouped.mean()
+ci = grouped.aggregate(lambda x: st.sem(x) * 1.96) #95% confidence interval
 
-print 'test'
+plotData = pd.DataFrame()
+plotData['Session'] = range(1,15)+range(1,15)
+plotData['Age'] = ['Young' for i in range(1,15)]+['Old' for i in range(1,15)]
+plotData['Mean'] = list(means.loc['Young']) + list(means.loc['Old'])
+plotData['CI'] = list(ci.loc['Young']) + list(ci.loc['Old'])
 
-#sns.jointplot(pd.Series(range(1,15)),0,inYoung)
-#inYoung['Session'] = range(1,15)
+sns.barplot(plotData)
+plt.show()
 
-#sns.jointplot()
+tups = [x for y in zip([(i,"Young") for i in range(1,15)],[(i,"Old") for i in range(1,15)]) for x in y]
+index = pd.MultiIndex.from_tuples(tups, names=['session', 'age'])
+
+
+
+
+
+
+
+
+
 
 
